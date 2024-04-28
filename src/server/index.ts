a6c9615bacd832ce495dcc7bb29bc365229eca0b
Noam Honig <noam.honig@gmail.com>
2024-04-28 13:03:22 +0300

    updated to esm

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,8 @@
-import express from "express"
-import { api } from "./api"
+import express from "express";
+import { api } from "./api.js";
 
-const app = express()
-app.use(api)
+const app = express();
+app.use(api);
 
 app.get("*", (req, res) => res.send(`api Server - path: "${req.path}"`))
-app.listen(3002, () => console.log("Server started"))
+app.listen(3002, () => console.log("Server started"));
